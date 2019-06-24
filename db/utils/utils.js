5d9840e684b29680b95b3b429fac65e02acca0fe
Chloe <c.williams22@hotmail.co.uk>
2019-06-24 16:41:47 +0100

    created and tested formatDate util function

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,9 @@
-exports.formatDate = list => {};
+exports.formatDate = list => {
+    return list.reduce((arr, item) => {
+      arr.push({...item, created_at: new Date(item['created_at'])})
+      return arr
+    }, [])
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,8 +1,85 @@
-const { expect } = require('chai');
-const { formatDate, makeRefObj, formatComments } = require('../db/utils/utils');
+const { expect } = require("chai");
+const { formatDate, makeRefObj, formatComments } = require("../db/utils/utils");
 
-describe('formatDate', () => {});
+describe("formatDate", () => {
+  it("returns a new empty array when passed an empty array", () => {
+    const testList = [];
+    const actual = formatDate(testList)
+    const expected = []
+      expect(actual).to.eql(expected)
+      expect(actual).to.not.equal(testList)
+  });
+  it('returns a new array of one object with the correctly formatted date', () => {
+    const testList = [{ 
+        body:"test body",
+        belongs_to: "test article",
+        created_by: 'test author',
+        votes: 1,
+        created_at: Date.now()
+      }];
+    const actual = formatDate(testList)
+    const expected = [{ 
+          body:"test body",
+          belongs_to: "test article",
+          created_by: 'test author',
+          votes: 1,
+          created_at: new Date()
+        }]
+        expect(actual).to.eql(expected)
+        expect(actual).to.not.equal(testList)
+  });
+  it('returns a new array of multiple objects with correctly formatted date', () => {
+    const testList = [{ 
+        body:"test body",
+        belongs_to: "test article",
+        created_by: 'test author',
+        votes: 1,
+        created_at: Date.now()
+      }, 
+      {
+      body:"test body 2",
+      belongs_to: "test article 2",
+      created_by: 'test author 2',
+      votes: 2,
+      created_at: Date.now()
+    }];    
+    const actual = formatDate(testList)
+    const expected = [{ 
+          body:"test body",
+          belongs_to: "test article",
+          created_by: 'test author',
+          votes: 1,
+          created_at: new Date()
+        }, 
+        {
+        body:"test body 2",
+        belongs_to: "test article 2",
+        created_by: 'test author 2',
+        votes: 2,
+        created_at: new Date()
+    }];
+        expect(actual).to.eql(expected)
+        expect(actual).to.not.equal(testList)
+  });
+  it('does not mutate the original array', () => {
+      const input = [{
+      body:"test body",
+      belongs_to: "test article",
+      created_by: 'test author',
+      votes: 1,
+      created_at: 1561390428698
+    }]
+      formatDate(input)
+      expect(input).to.eql([{
+        body:"test body",
+        belongs_to: "test article",
+        created_by: 'test author',
+        votes: 1,
+        created_at: 1561390428698
+      }])
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
